62cb392d65bd27dff14a5fa4d6352d86123635c7
gmanquilefn <g.manquilef.n@gmail.com>
2023-03-11 01:01:35 -0300

    initial commit

---
+++ src/main/java/cl/gmanquilefn/recordstore/RecordStoreApplication.java
@@ -0,0 +1,13 @@
+package cl.gmanquilefn.recordstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/gmanquilefn/recordstore/RecordstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.gmanquilefn.recordstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
